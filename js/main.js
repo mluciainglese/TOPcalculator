f3ab8e51ab0de3bb33bea659ed2917c8a614b0e5
Lucia Inglese <152501487+mluciainglese@users.noreply.github.com>
2025-04-04 14:03:21 +0100

    slected buttons and added event listeners to update display. Created functions to clear and delete input

--- js/main.js
+++ js/main.js
@@ -1,15 +1,69 @@
-function add(){
+let currentNumber = ""
+let operator = ""
+let output = ""
 
+// Basic calculation functions
+
+function add(a, b){
+    return a + b
 }
 
-function subtract(){
-
+function subtract(a, b){
+    return a - b
 }
 
-function multiply(){
-
+function multiply(a, b){
+    return a * b
 }
 
-function divide(){
+function divide(a, b){
+ return a / b
+}
 
+// Function to call the above functions
+
+function operate(a, op, b){
+    if (op === "+"){
+        return add(a, b)
+    } else if (op === "-"){
+        return subtract(a, b)
+    } else if (op === "*"){
+        return multiply(a, b)
+    } else if (op === "/"){
+        return divide(a, b)
+    } else {
+        return "Err"
+    }
+}
+
+// Update display when buttons are clicked
+
+let numButtons = document.querySelectorAll(".num")
+let opButtons = document.querySelectorAll(".op")
+let clear = document.querySelector(".clear")
+let del = document.querySelector(".del")
+let display = document.querySelector("#outputDisplay")
+
+numButtons.forEach(button =>{
+    button.addEventListener("click", updateDisplay)
+})
+
+opButtons.forEach(button =>{
+    button.addEventListener("click", updateDisplay)
+})
+
+clear.addEventListener("click", clearScreen)
+
+del.addEventListener("click", deleteLast)
+
+function updateDisplay(){
+    
+}
+
+function clearScreen(){
+    display.textContent = ""
+}
+
+function deleteLast(){
+    display.textContent = display.textContent.slice(0, -1)
 }
